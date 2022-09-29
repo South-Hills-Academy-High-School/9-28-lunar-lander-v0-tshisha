d6be4c02988b18e0986d60acfcc1c53ea7c57057
tshisha <113388366+tshisha@users.noreply.github.com>
2022-09-29 14:20:17 -0700

    fireball finish

--- main.ts
+++ main.ts
@@ -8,18 +8,40 @@ scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (s
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     apple.ay = -25 * Math.sin(angle)
     apple.ax = 25 * Math.cos(angle)
+    fireball.setFlag(SpriteFlag.Invisible, false)
 })
 controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
     angle += -15 * (3.14 / 180)
 })
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
     apple.ay = 20
+    fireball.setFlag(SpriteFlag.Invisible, true)
 })
 controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
     angle += 15 * (3.14 / 180)
 })
 let apple: Sprite = null
 let angle = 0
+let fireball: Sprite = null
+fireball = sprites.create(img`
+    . . . . . . 3 3 . . . . . . . . 
+    . . . . . . 3 1 3 . . . . . . . 
+    . . 3 3 . . 3 1 3 . . 3 3 . . . 
+    . . 3 1 3 . 3 1 3 2 3 1 3 . . . 
+    . . . 3 1 3 3 1 3 2 1 3 . . . . 
+    3 3 3 3 2 1 3 1 1 1 3 . . . . . 
+    3 1 1 1 1 1 1 1 1 2 3 3 3 3 3 3 
+    . 3 3 3 2 3 1 1 1 1 1 1 1 1 1 3 
+    . . . . . 2 1 1 1 3 3 2 3 3 3 . 
+    . . . . 3 1 3 1 3 1 2 . . . . . 
+    . . . 3 1 3 2 1 3 3 1 3 . . . . 
+    . . 3 1 3 . 2 1 3 . 3 1 3 . . . 
+    . . 3 3 . . 3 1 3 . . 3 3 . . . 
+    . . . . . . 3 1 3 . . . . . . . 
+    . . . . . . 3 1 3 . . . . . . . 
+    . . . . . . 3 3 . . . . . . . . 
+    `, SpriteKind.Projectile)
+fireball.setFlag(SpriteFlag.AutoDestroy, true)
 angle = 0
 tiles.setCurrentTilemap(tilemap`level1`)
 effects.starField.startScreenEffect()
@@ -56,5 +78,6 @@ game.onUpdate(function () {
     minimap.includeSprite(myminimap, apple)
     minimap2 = sprites.create(minimap.getImage(myminimap), SpriteKind.map)
     minimap2.setPosition(apple.x - 50, apple.x - 30)
-    engine.setPosition(apple.x - -8 * Math.cos(angle), apple.y - -8 * Math.sin(angle))
+    engine.setPosition(apple.x - -8 * Math.cos(angle), apple.y - (-8 + Math.sin(angle)))
+    fireball.setPosition(apple.x - -8 * Math.cos(angle), apple.y - (-8 + Math.sin(angle)))
 })
