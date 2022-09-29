5131510a6de48e28651f9c49cbd4a6a7206eb02a
tshisha <113388366+tshisha@users.noreply.github.com>
2022-09-29 13:50:15 -0700

    movement work

--- main.ts
+++ main.ts
@@ -3,19 +3,27 @@ namespace SpriteKind {
     export const rocketengine = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (sprite, location) {
-    mySprite.setVelocity(0, -1)
+    apple.setVelocity(0, -1)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.ay = -40
+    apple.ay = -25 * Math.sin(angle)
+    apple.ax = 25 * Math.cos(angle)
+})
+controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
+    angle += -15 * (3.14 / 180)
 })
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
-    mySprite.ay = 20
+    apple.ay = 20
 })
-let mySprite: Sprite = null
+controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
+    angle += 15 * (3.14 / 180)
+})
+let apple: Sprite = null
 let angle = 0
+angle = 0
 tiles.setCurrentTilemap(tilemap`level1`)
-effects.confetti.startScreenEffect()
-mySprite = sprites.create(img`
+effects.starField.startScreenEffect()
+apple = sprites.create(img`
     . . . . . . . e c 7 . . . . . . 
     . . . . e e e c 7 7 e e . . . . 
     . . c e e e e c 7 e 2 2 e e . . 
@@ -33,20 +41,20 @@ mySprite = sprites.create(img`
     . . . 2 2 e e 4 4 4 2 e e . . . 
     . . . . . 2 2 e e e e . . . . . 
     `, SpriteKind.Player)
-let mySprite3 = sprites.create(img`
+let engine = sprites.create(img`
     7 3 
     3 7 
     `, SpriteKind.rocketengine)
-scene.cameraFollowSprite(mySprite)
-scaling.scaleByPercent(mySprite, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
-mySprite.ay = 20
-let myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
-let mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
+scene.cameraFollowSprite(apple)
+scaling.scaleByPercent(apple, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+apple.ay = 20
+let myminimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
+let minimap2 = sprites.create(minimap.getImage(myminimap), SpriteKind.map)
 game.onUpdate(function () {
-    mySprite2.destroy()
-    myMinimap = minimap.minimap(MinimapScale.Half, 2, 0)
-    minimap.includeSprite(myMinimap, mySprite)
-    mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
-    mySprite2.setPosition(mySprite.left, mySprite.top)
-    mySprite.setPosition(mySprite.x, mySprite.y)
+    minimap2.destroy()
+    myminimap = minimap.minimap(MinimapScale.Half, 2, 0)
+    minimap.includeSprite(myminimap, apple)
+    minimap2 = sprites.create(minimap.getImage(myminimap), SpriteKind.map)
+    minimap2.setPosition(apple.x - 50, apple.x - 30)
+    engine.setPosition(apple.x - -8 * Math.cos(angle), apple.y - -8 * Math.sin(angle))
 })
